df44c0196af67866566aeefb2318766335ba5580
VictorDnB <victordnb@gmail.com>
2021-09-16 09:26:51 +0000

    fixed bug of price plugin

--- app/code/Unit1/Plugins/Plugin/AfterPricePlugin.php
+++ app/code/Unit1/Plugins/Plugin/AfterPricePlugin.php
@@ -4,7 +4,6 @@ namespace Unit1\Plugins\Plugin;
 
 class AfterPricePlugin{
     public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result){
-        $result = $result + 0.5;
+        return $result + 0.5;
     }
 }
-
